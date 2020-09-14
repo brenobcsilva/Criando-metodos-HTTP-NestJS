90f7f877dafefdffe97dad8431788a7304aaf40a
Breno Silva <breno.csilva@hotmail.com>
2020-09-13 22:12:16 -0300

    Criando provider

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
+import { Task.Service } from './shared/task.service';
 
 @Module({
-  controllers: [TasksController]
+  controllers: [TasksController],
+  providers: [Task.Service]
 })
 export class TasksModule {}
